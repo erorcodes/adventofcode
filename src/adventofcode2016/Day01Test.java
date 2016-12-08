e3e9b672d8734fc83b374e1ced93e5aca6a0a26d
Erika O <keiwra@gmail.com>
2016-12-08 23:42:23 +0100

    Solution to day 1, follow up task.
    Cleaned up the old code a bit as well. Still not prettiest, there's always things to improve, but I have more advent mysteries to solve!

--- src/adventofcode2016/Day01Test.java
+++ src/adventofcode2016/Day01Test.java
@@ -19,36 +19,52 @@ public class Day01Test extends TestCase {
         instr = "R1";
         santa.followInstructions(instr);
         assertEquals("east", santa.direction);
-        assertEquals(1, santa.getDistance());
+        assertEquals(1, santa.getDistanceToEasterBunnyHQ());
     }
 
     public void testSantaMovesLeftOneBlock() {
         instr = "L1";
         santa.followInstructions(instr);
         assertEquals("west", santa.direction);
-        assertEquals(1, santa.getDistance());
+        assertEquals(1, santa.getDistanceToEasterBunnyHQ());
     }
 
     public void testSantaMovesRightThreeBlocks() {
         instr = "R3";
         santa.followInstructions(instr);
         assertEquals("east", santa.direction);
-        assertEquals(3, santa.getDistance());
+        assertEquals(3, santa.getDistanceToEasterBunnyHQ());
     }
 
     public void testRightOneBlockThenLeftOneBlock() {
         instr = "R1, L1";
         santa.followInstructions(instr);
         assertEquals("north", santa.direction);
-        assertEquals(2, santa.getDistance());
+        assertEquals(2, santa.getDistanceToEasterBunnyHQ());
     }
 
+    public void testEndingUpOnAlreadyPassedCrossing() {
+        instr = "R2, R1, R1, R1";
+        santa.followInstructions(instr);
+        assertEquals(1, santa.getRealDistanceToEasterBunnyHQ());
+    }
 
+    public void testPassingAlreadyPassedCrossing() {
+        instr = "R8, R4, R4, R8";
+        santa.followInstructions(instr);
+        assertEquals(4, santa.getRealDistanceToEasterBunnyHQ());
+    }
 
+/* Tests to get final answers*/
 
-    public void testFinalSantaFindsEasterBunny() {
+    public void testSantaFindsEasterBunnyHQ() {
         instr = "L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L192, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R188, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2";
         santa.followInstructions(instr);
-        assertEquals(226, santa.getDistance());
+
+/* Remove commenting here and run tests to get answers!
+
+        assertEquals(0, santa.getDistanceToEasterBunnyHQ());
+        assertEquals(0, santa.getRealDistanceToEasterBunnyHQ());
+*/
     }
 }
--- src/adventofcode2016/Santa.java
+++ src/adventofcode2016/Santa.java
@@ -1,13 +1,21 @@
 package adventofcode2016;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Santa {
     public String direction = "north";
     public int x = 0;
     public int y = 0;
+    private List<String> visitedCrossings = new ArrayList<String>();
+    private List<String> crossingsVisitedMultipleTimes = new ArrayList<String>();
+    private String currentPosition;
+    private int distanceToEasterBunnyHQ = 0;
 
     public void followInstructions (String instr) {
 
         String[] arrayOfInstructions = instr.split(", ");
+
         for (int i = 0; i < arrayOfInstructions.length; i++) {
 
             String instruction = arrayOfInstructions[i];
@@ -16,7 +24,7 @@ class Santa {
             int blocks = Integer.parseInt(instruction.substring(1));
 
             changeDirection(turn);
-            walk(blocks);
+            walkBlocks(blocks);
         }
     }
 
@@ -46,19 +54,39 @@ class Santa {
         }
     }
 
-    private void walk (int blocks) {
-        if (direction.equals("west")) {
-            x -= blocks;
-        } else if (direction.equals("north")) {
-            y += blocks;
-        } else if (direction.equals("east")) {
-            x += blocks;
-        } else if (direction.equals("south")) {
-            y -= blocks;
+    private void walkBlocks (int blocks) {
+        for (int i = 0; i < blocks; i++) {
+            if (direction.equals("west")) {
+                x -= 1;
+            } else if (direction.equals("north")) {
+                y += 1;
+            } else if (direction.equals("east")) {
+                x += 1;
+            } else if (direction.equals("south")) {
+                y -= 1;
+            }
+
+            currentPosition = x+","+y;
+
+            if (visitedCrossings.contains(currentPosition) == true) {
+                if (crossingsVisitedMultipleTimes.contains(currentPosition) == false) {
+                    crossingsVisitedMultipleTimes.add(currentPosition);
+                    if (distanceToEasterBunnyHQ == 0) {
+                        distanceToEasterBunnyHQ = getDistanceToEasterBunnyHQ();
+                    }
+                }
+            }
+            else {
+                visitedCrossings.add(currentPosition);
+            }
         }
     }
 
-    public int getDistance () {
+    public int getDistanceToEasterBunnyHQ () {
         return Math.abs(x) + Math.abs(y);
+    }
+
+    public int getRealDistanceToEasterBunnyHQ () {
+        return distanceToEasterBunnyHQ;
     }
 }
