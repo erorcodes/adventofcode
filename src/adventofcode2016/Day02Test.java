a88057ca945b05503b3b0c13d7891f6df4962031
Erika O <keiwra@gmail.com>
2016-12-09 09:33:12 +0100

    Stupid test for getting a combination. Won't work when each instruction for a key is longer than one move.

--- src/adventofcode2016/Day02Test.java
+++ src/adventofcode2016/Day02Test.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 public class Day02Test extends TestCase{
 
     private KeyPad keyPad;
+    private String instr;
 
     public void setUp() {
         keyPad = new KeyPad();
@@ -12,5 +13,35 @@ public class Day02Test extends TestCase{
 
     public void testStartKeyIsFive() {
         assertEquals(5, keyPad.key);
+    }
+
+    public void testOnceUpIsTwo() {
+        instr = "U";
+        keyPad.pressKey(instr);
+        assertEquals(2, keyPad.key);
+    }
+
+    public void testTwoTimesUp() {
+        instr = "UU";
+        keyPad.pressKey(instr);
+        assertEquals(2, keyPad.key);
+    }
+
+    public void testUpDownLeftLeftRight() {
+        instr = "UDLLR";
+        keyPad.pressKey(instr);
+        assertEquals(5, keyPad.key);
+    }
+
+    public void testLeftDownDownDown() {
+        instr = "LDDD";
+        keyPad.pressKey(instr);
+        assertEquals(7, keyPad.key);
+    }
+
+    public void testTwoKeys() {
+        instr = "U,L";
+        keyPad.pressKey(instr);
+        assertEquals("21", keyPad.combination);
     }
 }
--- src/adventofcode2016/KeyPad.java
+++ src/adventofcode2016/KeyPad.java
@@ -2,4 +2,115 @@ package adventofcode2016;
 
 public class KeyPad {
     public int key = 5;
+    public String combination = "";
+
+    public void pressKey (String instruction) {
+        for (int i = 0; i < instruction.length(); i++) {
+            String nextKey = String.valueOf(instruction.charAt(i));
+
+            if (nextKey.equals("U")) {
+                switch (key) {
+                    case 1:
+                    case 4:
+                        key = 1;
+                        break;
+                    case 2:
+                    case 5:
+                        key = 2;
+                        break;
+                    case 3:
+                    case 6:
+                        key = 3;
+                        break;
+                    case 7:
+                        key = 4;
+                        break;
+                    case 8:
+                        key = 5;
+                        break;
+                    case 9:
+                        key = 6;
+                        break;
+                }
+                combination += key;
+            } else if (nextKey.equals("D")) {
+                switch (key) {
+                    case 1:
+                        key = 4;
+                        break;
+                    case 2:
+                        key = 5;
+                        break;
+                    case 3:
+                        key = 6;
+                        break;
+                    case 4:
+                    case 7:
+                        key = 7;
+                        break;
+                    case 5:
+                    case 8:
+                        key = 8;
+                        break;
+                    case 6:
+                    case 9:
+                        key = 9;
+                        break;
+                }
+                combination += key;
+            } else if (nextKey.equals("L")) {
+                switch (key) {
+                    case 1:
+                    case 2:
+                        key = 1;
+                        break;
+                    case 3:
+                        key = 2;
+                        break;
+                    case 4:
+                    case 5:
+                        key = 4;
+                        break;
+                    case 6:
+                        key = 5;
+                        break;
+                    case 7:
+                    case 8:
+                        key = 7;
+                        break;
+                    case 9:
+                        key = 8;
+                        break;
+                }
+                combination += key;
+            } else if (nextKey.equals("R")) {
+                switch (key) {
+                    case 1:
+                        key = 2;
+                        break;
+                    case 2:
+                    case 3:
+                        key = 3;
+                        break;
+                    case 4:
+                        key = 5;
+                        break;
+                    case 5:
+                    case 6:
+                        key = 6;
+                        break;
+                    case 7:
+                        key = 8;
+                        break;
+                    case 8:
+                    case 9:
+                        key = 9;
+                        break;
+                }
+                combination += key;
+            }
+        }
+
+    }
+
 }
