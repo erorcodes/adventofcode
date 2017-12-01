070c3e7ded154497f35603a83a886535b60715a4
Erika Ornstein <keiwra@gmail.com>
2017-12-01 13:20:22 +0100

    Add second test and the corresponding code to pass test

--- src/main/java/adventofcode2017/CaptchaPad.java
+++ src/main/java/adventofcode2017/CaptchaPad.java
@@ -3,6 +3,21 @@ package adventofcode2017;
 public class CaptchaPad {
 
     public int sumOfEqualNumbers(String captcha) {
-        return 3;
+        int sum = 0;
+        for (int i = 0; i < captcha.length(); i++) {
+            int firstNumber = Integer.parseInt(String.valueOf(captcha.charAt(i)));
+            if (i == captcha.length() - 1) {
+                if (firstNumber == Integer.parseInt(String.valueOf(captcha.charAt(0)))){
+                    sum += firstNumber;
+                }
+                break;
+            }
+            int secondNumber = Integer.parseInt(String.valueOf(captcha.charAt(i + 1)));
+
+            if (firstNumber == secondNumber) {
+                sum += firstNumber;
+            }
+        }
+        return sum;
     }
 }
--- src/test/java/adventofcode2017/Day01Test.java
+++ src/test/java/adventofcode2017/Day01Test.java
@@ -4,11 +4,18 @@ import junit.framework.TestCase;
 
 public class Day01Test extends TestCase {
 
-    public void test1122ShouldBe3() throws Exception {
+    public void test1122ShouldReturn3() throws Exception {
         String captcha = "1122";
         CaptchaPad captchaPad = new CaptchaPad();
 
         assertEquals(3, captchaPad.sumOfEqualNumbers(captcha));
+    }
+
+    public void test1111ShouldReturn4() throws Exception {
+        String captcha = "1111";
+        CaptchaPad captchaPad = new CaptchaPad();
+
+        assertEquals(4, captchaPad.sumOfEqualNumbers(captcha));
     }
 
 }
