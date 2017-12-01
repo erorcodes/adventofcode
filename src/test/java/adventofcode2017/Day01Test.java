7bb988d58011c40f83b59bdf33363f308a1655eb
Erika Ornstein <keiwra@gmail.com>
2017-12-01 13:25:37 +0100

    Refactor test class

--- src/test/java/adventofcode2017/Day01Test.java
+++ src/test/java/adventofcode2017/Day01Test.java
@@ -4,17 +4,20 @@ import junit.framework.TestCase;
 
 public class Day01Test extends TestCase {
 
-    public void test1122ShouldReturn3() throws Exception {
-        String captcha = "1122";
-        CaptchaPad captchaPad = new CaptchaPad();
+    private String captcha;
+    private CaptchaPad captchaPad;
 
+    protected void setUp() {
+        captchaPad = new CaptchaPad();
+    }
+
+    public void test1122ShouldReturn3() throws Exception {
+        captcha = "1122";
         assertEquals(3, captchaPad.sumOfEqualNumbers(captcha));
     }
 
     public void test1111ShouldReturn4() throws Exception {
-        String captcha = "1111";
-        CaptchaPad captchaPad = new CaptchaPad();
-
+        captcha = "1111";
         assertEquals(4, captchaPad.sumOfEqualNumbers(captcha));
     }
 
