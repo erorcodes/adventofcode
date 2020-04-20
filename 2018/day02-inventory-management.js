d21ce3f27cec27dc69feb9444276dc722464ad2a
Erika Ornstein <keiwra@gmail.com>
2020-04-20 13:12:09 +0200

    Day 2 part 1 finished! Skipped object and kept to arrays.

--- 2018/day02-inventory-management.js
+++ 2018/day02-inventory-management.js
@@ -3,38 +3,35 @@ const day2Input = fs.readFileSync("./input-day02.txt").toString('utf-8');
 
 const inventoryManager = (input) => {
   const inputAsArray = input.split("\n").map(String);
+  // console.log(inputAsArray);
+
   let two = 0;
   let three = 0;
-  
-  inputAsArray.forEach((id) => {
-    const idAsArray = id.split("").map(String);
-    // console.log(idAsArray);
-    const count = Object.create(null);
-    
-    idAsArray.forEach(letter => {
-      count[letter] = count[letter] ? count[letter] + 1 : 1;
-      console.log(count);
+
+  inputAsArray.forEach((boxId) => {
+    let occurrences = [];
+
+    // Make string into array
+    const boxIdAsArray = boxId.split("");
+    // console.log(boxIdAsArray);
+
+    // Check for each letter in the string how many times it occur
+    boxIdAsArray.forEach(letter => {
+      let letterCount = boxIdAsArray.filter(l => l === letter).length;
+      letterCount > 1 && occurrences.push(letterCount);
     });
-    console.log(count);
 
 
-    for (let letter of Object.keys(count)) {
-      if (count[letter] === 2) {
-        two += 1;
-        console.log("Adding a " + letter + " to 2")
-      }
-    }
+    // console.log('occurrences: ' + occurrences);
 
-    for (let letter of Object.keys(count)) {
-      if (count[letter] === 3) {
-        three += 1;
-        console.log("Adding a " + letter + " to 3")
-      }
-    }
+    // Check if the array contains at least one of 2 and/or 3, if so add it to the count
+    occurrences.includes(2) && two++;
+    occurrences.includes(3) && three++;
+
   });
   console.log("Two: " + two);
   console.log("Three: " + three);
-  console.log(two * three);
+  // console.log("Two * Three is: " + two * three);
   return two * three;
 };
 
@@ -48,6 +45,6 @@ abcdee
 ababab`;
 
 // TESTS, VERY BASIC
-console.log(inventoryManager(testCase) === 12);
+console.log("Test case: " + (inventoryManager(testCase) === 12));
 
-// console.log(inventoryManager(day2Input));
+console.log("With day 2 input: " + inventoryManager(day2Input));
