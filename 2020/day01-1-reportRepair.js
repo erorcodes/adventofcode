ed43abde79a9a884ca76ebc0795a8a42edc54d1d
Erika Ornstein <keiwra@gmail.com>
2020-12-01 22:29:30 +0100

    Solution for part 2 of day 1 added.

--- 2020/day01-1-reportRepair.js
+++ 2020/day01-1-reportRepair.js
@@ -1,7 +1,7 @@
 
-const checkSum = (numbers) => numbers.reduce((acc, curr) => acc + curr);
+const checkSum = (numbers) => numbers.reduce((acc, curr) => acc + curr) === 2020;
 
-function reportRepair(input) {
+function reportRepair(input, n) {
   let arr = input.split("\n").map(Number);
   let numbers = [];
 
@@ -13,9 +13,19 @@ function reportRepair(input) {
       // Add A and B
       numbers[1] = arr[j];
 
-      // Check if sum is 2020
-      if (checkSum(numbers) === 2020) {
-        return numbers[0] * numbers[1];
+      if (n === 2) {
+        // Check if sum is 2020
+        if (checkSum(numbers)) {
+          return numbers[0] * numbers[1];
+        }
+      } else if (n === 3) {
+        for (let k = 2; k < arr.length; k++) {
+          numbers[2] = arr[k];
+
+          if (checkSum(numbers)) {
+            return numbers[0] * numbers[1] * numbers[2];
+          }
+        }
       }
 
     }
@@ -39,9 +49,10 @@ let testCase2 =
   299
   1456`; // 514579
 
-console.log('Test case 1: ' + reportRepair(testCase1));
-console.log('Test case 2: ' + reportRepair(testCase2));
+console.log('Test case 1: ' + reportRepair(testCase1, 2));
+console.log('Test case 2: ' + reportRepair(testCase2, 2));
 
 const fs = require("fs");
 const day01input = fs.readFileSync("./day01.txt").toString('utf-8');
-console.log('Day 1: ' + reportRepair(day01input));
+console.log('Day 1, part 1: ' + reportRepair(day01input, 2));
+console.log('Day 1, part 2: ' + reportRepair(day01input, 3));
