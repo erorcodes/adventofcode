31043e8e9db861a57357794eec935aa868a8a43d
Erika Ornstein <keiwra@gmail.com>
2020-12-09 14:32:43 +0100

    2020: Solution to day 6 part 2

--- 2020/day06-customCustoms.js
+++ 2020/day06-customCustoms.js
@@ -1,15 +1,32 @@
-const splittedInput = input => input.split(/\n\n/).map(i => i.split(/\n/)).map(e => {
+const groupedAnswers = allAnswers => allAnswers.split(/\n\n/).map(i => i.split(/\n/));
+
+const uniqueAnswers = allAnswers => groupedAnswers(allAnswers).map(answer => {
   let arr = [];
-  for (let i = 0; i < e.length; i++) {
-    e[i].length > 1 ? e[i].split("").forEach(t => arr.push(t)) : arr.push(e[i]);
+  for (let i = 0; i < answer.length; i++) {
+    answer[i].length > 1 ? answer[i].split("").forEach(t => arr.push(t)) : arr.push(answer[i]);
   }
   return arr;
-});
+}).map(a => [...new Set(a)]).map(a => a.length).reduce((prev, curr) => prev + curr, 0);
 
-const countedArray = arr => arr.map(a => [...new Set(a)]).map(a => a.length).reduce((prev, curr) => prev + curr, 0);
+const commonAnswers = allAnswers => {
+  let common = 0;
+  groupedAnswers(allAnswers).map(a => a.map(ans => ans.split("")))
+    .forEach(group => {
+    let checkedQuestions = [];
+    for (const answerSheet of group) {
+      for (const question of answerSheet) {
+        if (!checkedQuestions.includes(question) && group.every((ans) => ans.includes(question))) {
+          common++;
+        }
+        checkedQuestions.push(question);
+      }
+    }
+  });
+  return common;
+};
 
 let testCase1 =
-  `abc
+`abc
 
 a
 b
@@ -24,8 +41,11 @@ a
 a
 
 b`;
-console.log("Test case part 1: " + (countedArray(splittedInput(testCase1)) === 11));
+console.log("Test case part 1: " + (uniqueAnswers(testCase1) === 11));
+// console.log("Test case part 2: " + commonAnswers(testCase1));
+console.log("Test case part 2: " + (commonAnswers(testCase1) === 6));
 
 const fs = require("fs");
 const puzzleInput = fs.readFileSync("./day06.txt").toString('utf-8');
-console.log("Puzzle of the day, part 1: " + countedArray(splittedInput(puzzleInput)));
+console.log("Puzzle of the day, part 1: " + uniqueAnswers(puzzleInput));
+console.log("Puzzle of the day, part 2: " + commonAnswers(puzzleInput));
